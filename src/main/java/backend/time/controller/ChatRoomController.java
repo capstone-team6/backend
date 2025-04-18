7435459b336d18eda30b69a63a3a17c4bb110a92
jimmy0524 <10jmin04@naver.com>
2025-04-19 01:30:26 +0900

    250419 jm delete BoardServiceImpl

--- src/main/java/backend/time/controller/ChatRoomController.java
+++ src/main/java/backend/time/controller/ChatRoomController.java
@@ -10,7 +10,7 @@ import backend.time.model.ChatType;
 import backend.time.model.Member.Member;
 import backend.time.model.board.Board;
 import backend.time.model.board.BoardType;
-import backend.time.service.BoardServiceImpl;
+import backend.time.service.BoardService;
 import backend.time.service.ChattingService;
 import backend.time.service.MemberService;
 import backend.time.service.NotificationService;
@@ -19,12 +19,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RestController
@@ -35,7 +33,7 @@ public class ChatRoomController {
 
     private final ChattingService chattingService;
     private final MemberService memberService;
-    private final BoardServiceImpl boardService;
+    private final BoardService boardService;
     private final NotificationService notificationService;
     private final EntityManager entityManager;
 
